e8eadcc9904175cd98063ba2100a9aef670c5444
icaparros-at-wiris <isaac.caparros@wiris.com>
2023-06-20 08:50:16 +0200

    new changes

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<!-- App.vue -->
+
+<template>
+    <div id="app">
+        <ckeditor :editor="editor" v-model="editorData" :config="editorConfig"></ckeditor>
+    </div>
+</template>
+
+<script>
+    // Since we're building CKEditor 5 from source, we use the source version of ClassicEditor.
+    import { ClassicEditor } from '@ckeditor/ckeditor5-editor-classic';
+
+    import { Essentials } from '@ckeditor/ckeditor5-essentials';
+    import { Bold, Italic } from '@ckeditor/ckeditor5-basic-styles';
+    import { Link } from '@ckeditor/ckeditor5-link';
+    import { Paragraph } from '@ckeditor/ckeditor5-paragraph';
+    import MathType from '@wiris/mathtype-ckeditor5/src/plugin';
+  
+    export default {
+        name: 'app',
+        data() {
+            return {
+                editor: ClassicEditor,
+                editorData: '<p>Content of the editor.</p>',
+                editorConfig: {
+                    plugins: [
+                        Essentials,
+                        Bold,
+                        Italic,
+                        Link,
+                        Paragraph,
+                        MathType
+                    ],
+
+                    toolbar: {
+                        items: [
+                            'bold',
+                            'italic',
+                            'link',
+                            'undo',
+                            'redo',
+                            'MathType',
+                            'ChemType',
+                        ]
+                    }
+                },
+            };
+        }
+    };
+</script>
